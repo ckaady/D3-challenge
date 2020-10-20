2b4624d12baafe4b429a925baf41c54746c36817
Christina Kaady <christina.kaady@gmail.com>
2020-10-19 17:11:07 -0700

    app progress

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -29,10 +29,10 @@ var chosenXAxis = "poverty";
 var chosenYAxis = "healthcare";
 
 // Retrieve data from the CSV file 
-d3.csv("./Assets/data/data.csv").then(incData => {
+d3.csv("assets/data/data.csv").then(censusData => {
 
     // parse data
-    incData.forEach(data => {
+    censusData.forEach(data => {
         data.poverty = +data.poverty;
         data.age = +data.age;
         data.income = +data.income;
@@ -40,3 +40,110 @@ d3.csv("./Assets/data/data.csv").then(incData => {
         data.smokes = +data.smokes;
         data.healthcare = +data.healthcare;
     });
+
+    // console.log(incData);
+
+    // function used for updating x-scale var upon click on axis label
+    function xScale(censusData, chosenXAxis) {
+        // create scales
+        var xLinearScale = d3.scaleLinear()
+          .domain([d3.min(censusData, d => d[chosenXAxis]) * 0.8,
+            d3.max(censusData, d => d[chosenXAxis]) * 1.2
+          ])
+          .range([0, width]);
+      
+        return xLinearScale;
+      
+      }
+    
+    // function used for updating xAxis var upon click on axis label
+    function renderAxes(newXScale, xAxis) {
+    var bottomAxis = d3.axisBottom(newXScale);
+  
+    xAxis.transition()
+      .duration(1000)
+      .call(bottomAxis);
+  
+    return xAxis;
+  }
+  
+    // function used for updating circles group with a transition to
+    // new circles
+    function renderCircles(circlesGroup, newXScale, chosenXAxis) {
+    
+      circlesGroup.transition()
+        .duration(1000)
+        .attr("cx", d => newXScale(d[chosenXAxis]));
+    
+      return circlesGroup;
+    }
+
+    // function used for updating circles group with new tooltip
+    function updateToolTip(chosenXAxis, circlesGroup) {
+    
+      var label;
+    
+      if (chosenXAxis === "poverty") {
+        label = "In Poverty (%)";
+      }
+    //   else {
+    //     label = "# of Albums:";
+    //   }
+    
+      var toolTip = d3.tip()
+        .attr("class", "tooltip")
+        .offset([80, -60])
+        .html(d => `${d.abbr}<br>${label} ${d[chosenXAxis]}`);
+    
+      circlesGroup.call(toolTip);
+    
+      circlesGroup.on("mouseover", function(data) {
+          toolTip.show(data);
+        })
+        // onmouseout event
+        .on("mouseout", function(data) {
+          toolTip.hide(data);
+        });
+      
+      return circlesGroup;
+    }
+  
+    
+
+    // xLinearScale function above csv import
+    var xLinearScale = xScale(censusData, chosenXAxis);
+
+    // Create y scale function
+    var yLinearScale = (censusData, chosenYAxis);
+
+    // Create initial axis functions
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+
+    // append x axis
+    var xAxis = chartGroup.append("g")
+      .classed("x-axis", true)
+      .attr("transform", `translate(0, ${height})`)
+      .call(bottomAxis);
+
+    // append y axis
+    chartGroup.append("g")
+      .call(leftAxis);
+
+    // append initial circles
+    var circlesGroup = chartGroup.selectAll("circle")
+      .data(hairData)
+      .join("circle")
+      .attr("cx", d => xLinearScale(d[chosenXAxis]))
+      .attr("cy", d => yLinearScale(d.healthcare))
+      .attr("r", 20)
+      .attr("fill", "pink")
+      .attr("opacity", 0.5)
+      .attr("stroke", "black");
+
+
+})
+
+    
+
+
